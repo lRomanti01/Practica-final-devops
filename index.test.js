533580b20f85bc1b453a5e840aad1e25634e1551
lRomanti01 <Romanti159@gmail.com>
2023-12-16 20:20:05 -0400

    refactor: :bug: delete mocking

--- index.test.js
+++ index.test.js
@@ -1,12 +1,5 @@
 const { getMessage, cambiarMensaje } = require('./index');
 const fs = require('fs');
-const { JSDOM } = require('jsdom');
-
-// Mockear las funciones de document y window para Jest
-const html = fs.readFileSync('index.html', 'utf-8'); 
-const { window } = new JSDOM(html, { runScripts: 'dangerously' });
-global.document = window.document;
-global.window = window;
 
 test('Comprobación del mensaje ', () => {
   expect(getMessage()).toBe("Mensaje de prueba para la practica final");
